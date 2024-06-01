075086844e113192fcd0a8c2c4f5387f7109c362
wallace <w.wallacebarros.cruz@gmail.com>
2024-06-01 19:08:21 -0300

    implementando o component Button

---
+++ front-end/src/view/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button({text}) {
+  return(
+  <button className="bg-primary-400 py-1 border-primary-200 border-2 text-primary-100 text-2xl m-auto min-w-40 px-4 rounded-lg">
+    {text}
+  </button>
+  )
+}
