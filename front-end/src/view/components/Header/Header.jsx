ab969550f5240abd5bf0f883a0db0fab148f8693
wallace <w.wallacebarros.cruz@gmail.com>
2024-06-01 19:07:43 -0300

    implementando o component Header

---
+++ front-end/src/view/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return(
+  <header className="bg-primary-300 py-5 flex">
+    <h1 className="text-3xl text-primary-100 font-medium mx-auto">
+      CONVERSOR DE NÚMEROS
+    </h1>
+  </header>
+  )
+}
