15faf3988ab3ad2d4c170b5d521fb9f63d0ffe7d
wallace <w.wallacebarros.cruz@gmail.com>
2024-06-05 22:56:06 -0300

    fix: Implementando o select no main

--- front-end/src/view/components/Main/Main.jsx
+++ front-end/src/view/components/Main/Main.jsx
@@ -1,14 +1,16 @@
 import Input from "../Input/Input"
 import Button from "../Button/Button"
+import Select from "../Select/Select"
 
 export default function Main() {
+  const value = ["binary", "octal", "decial", "hexadecimal"]
   return(
   <main className="bg-primary-300 m-8 my-auto p-8 py-16 border-2 border-primary-200 rounded-md flex flex-col gap-4 justify-between">
     <Input placeholder="0" id="valor" label="Valor"/>
-    <Input placeholder="" id="entrada" label="Converter de:"/>
+    <Select id={"para"} name={"Para:"} values={value}/>
     <Button text="Inverter"/>
-    <Input placeholder="" id="saida" label="Para:"/>
-    <Button text="Converter" />
+    <Select id={"para"} name={"Para:"} values={value}/>
+    <Button text="Converter"/>
     <Input placeholder="0" id="convercao" label="Valor convertido:"/>
   </main>
   )
